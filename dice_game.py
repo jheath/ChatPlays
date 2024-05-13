2fb4156cb1f1d1d61d62d4e80074a11e9e3e9b73
jchalos <jchalos@gmail.com>
2024-05-13 16:20:16 -0500

    Added dice_game.py from SCARpupGaming

--- dice_game.py
+++ dice_game.py
@@ -2,10 +2,12 @@ import sys
 import random
 
 # var inits
-dice = [0, 0, 0, 0, 0]
-diceHeld = ["Not Held", "Not Held", "Not Held", "Not Held", "Not Held"]
+heldText = "Held"
+notHeldText = "Not Held"
+dice[:5] = 0
+diceHeld[:5] = notHeldText
+
 rollsLeft = 3
-gameState = "new roll"
 userScore = 0
 playing = True
 
@@ -49,16 +51,14 @@ def user_check_dice():
 
         if val == "C" or val == 'c':
             userDone = True
-        elif val == "1" or val == "2" or val == "3" or val == "4" or val == "5":
-            if diceHeld[int(val) - 1] == "Not Held":
-                diceHeld[int(val) - 1] = "Held"
-            else:
-                diceHeld[int(val) - 1] = "Not Held"
+        elif val == range(1:6):
+            if diceHeld[int(val) - 1] == notHeldText:
+                diceHeld[int(val) - 1] = heldValue
 
 
 def reroll():
     for x in range(5):
-        if diceHeld[x] == "Not Held":
+        if diceHeld[x] == notHeldText:
             dice[x] = random.randrange(1, 7)
             continue
     reduceRolls()
@@ -77,54 +77,36 @@ def resetRolls():
 def resetHelds():
     global diceHeld
     for x in range(5):
-        diceHeld[x] = "Not Held"
+        diceHeld[x] = notHeldText
 
 
 def score_roll():
     score = 0
-    num1 = 0
-    num2 = 0
-    num3 = 0
-    num4 = 0
-    num5 = 0
-    num6 = 0
+    num[:6] = 0
     for x in range(5):
         print('Dice #' + str(x + 1) + ': ' + str(dice[x]))
 
-    # check for yahtzee
-    if dice[0] == dice[1] and dice[1] == dice[2] and dice[2] == dice[3] and dice[3] == dice[4]:
+    # check for straights
+    # looks right, range may be wrong -erg
+    count = 0
+    for x in range(1:5):
+        if dice[num[x]] > dice[num[x -1]]
+            count += 1
+        else
+            count = 0
+        # initial score
+        score += dice[x]
+
+    # small straight
+    if count >= 2:
+        score = 30
+    # Large straight
+    elif count > 3:
+        score = 40
+    # Yahtzee
+    elif count > 4:
         score = 50
 
-    # check for straights
-    for x in range(5):
-        if dice[x] == 1:
-            num1 += 1
-        elif dice[x] == 2:
-            num2 += 1
-        elif dice[x] == 3:
-            num3 += 1
-        elif dice[x] == 4:
-            num4 += 1
-        elif dice[x] == 5:
-            num5 += 1
-        elif dice[x] == 6:
-            num6 += 1
-
-    # Large Straight check
-    if (num1 > 0 and num2 > 0 and num3 > 0 and num4 > 0 and num5 > 0) or (
-            num2 > 0 and num3 > 0 and num4 > 0 and num5 > 0 and num6 > 0):
-        score = 40
-
-    # Small Straight check
-    if score == 0 and (
-            (num1 > 0 and num2 > 0 and num3 > 0 and num4 > 0)
-            or (num2 > 0 and num3 > 0 and num4 > 0 and num5 > 0)
-            or (num3 > 0 and num4 > 0 and num5 > 0 and num6 > 0)):
-        score = 30
-
-    if score == 0:
-        for x in range(5):
-            score += dice[x]
 
     # check for full house and set to 25 if it is less than the current score
     if score < 25:
