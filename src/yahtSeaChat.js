1094a34ed456c8a5877a8d3bd2ae3f9369f10b15
botscheduling <100870449+botscheduling@users.noreply.github.com>
2024-06-03 21:11:33 -0700

    Updated yahtsea chat to process from chat and not the command line.

--- src/yahtSeaChat.js
+++ src/yahtSeaChat.js
@@ -1,6 +1,14 @@
 const { execFile } = require('child_process');
 
-function processChat(username) {
+function processChat(message, username) {
+	const [action, target] = message.trim().toLowerCase().split(' ', 2);
+	executeCall(username, action, target, (error, result) => {
+		process.stdout.write(`${result.message}\n`);
+	});
+}
+
+//TODO - This function is incomplete and needs to be implemented
+function processCommand(m) {
 	const readline = require('readline');
 
 	const rl = readline.createInterface({
@@ -18,7 +26,6 @@ function processChat(username) {
 
 function executeCall(username, action, target, callback) {
 	execFile('python', ['dice_game.py', username, action, target], (error, stdout, stderr) => {
-
 		const jsonStr = stdout.replace(/'/g, '"');
 		callback(null, JSON.parse(jsonStr));
 	});
